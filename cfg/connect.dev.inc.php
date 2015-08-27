573b038ed71e9e6c3ccc33387f20d2da49c947d6
Ruslan Dobrovolskyy <rusdobr@gmail.com>
2015-08-27 20:47:20 +0300

    Db Connect dev-prod

--- cfg/connect.dev.inc.php
+++ cfg/connect.dev.inc.php
@@ -8,7 +8,4 @@
 	define('ADMIN_LOGIN', 'YWRtaW4='); //administrator's login
 	define('ADMIN_PASS', '21232f297a57a5a743894a0e4a801fc3'); //administrator's login
 
-	//database tables
-	include("./cfg/tables.inc.php");
-
 ?>
