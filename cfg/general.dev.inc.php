84d6df9ce310c60b7c97a12c7cb8628cd3eb3ef7
Ruslan Dobrovolskyy <rusdobr@gmail.com>
2015-08-30 21:55:42 +0300

    UTF-8 encoding provided

--- cfg/general.dev.inc.php
+++ cfg/general.dev.inc.php
@@ -1,8 +1,8 @@
 <?php
 	define('CONF_SHOP_NAME', 'Мой интернет-магазин');
 	define('CONF_SHOP_URL', 'www.mystore.ru');
-	define('CONF_GENERAL_EMAIL', 'your@email.ru');
-	define('CONF_ORDERS_EMAIL', 'your@email.ru');
+	define('CONF_GENERAL_EMAIL', 'dobrovolska.oksana@gmail.com');
+	define('CONF_ORDERS_EMAIL', 'dobrovolska.oksana@gmail.com');
 	define('CONF_CURRENCY_ID_LEFT', '$');
 	define('CONF_CURRENCY_ID_RIGHT', '');
 	define('CONF_CURRENCY_ISO3', 'USD');
--- cfg/general.inc.php
+++ cfg/general.inc.php
@@ -1,8 +1,8 @@
 <?php
 	define('CONF_SHOP_NAME', 'Мой интернет-магазин');
 	define('CONF_SHOP_URL', 'www.mystore.ru');
-	define('CONF_GENERAL_EMAIL', 'your@email.ru');
-	define('CONF_ORDERS_EMAIL', 'your@email.ru');
+	define('CONF_GENERAL_EMAIL', 'dobrovolska.oksana@gmail.com');
+	define('CONF_ORDERS_EMAIL', 'dobrovolska.oksana@gmail.com');
 	define('CONF_CURRENCY_ID_LEFT', '&#8372;');
 	define('CONF_CURRENCY_ID_RIGHT', 'грн.');
 	define('CONF_CURRENCY_ISO3', 'UAH');
