6fe03dad26b6618efdee8ffce56d4e2c70798ecf
Ruslan <rusdobr@gmail.com>
2016-09-21 08:51:34 +0300

    Create vagrant environment

--- cfg/connect.inc.php
+++ cfg/connect.inc.php
@@ -1,11 +1,13 @@
 <?php
 	//database connection settings
 
-    if ($_SERVER['HTTP_HOST'] == 'localhost') {
-        include("./cfg/connect.dev.inc.php");
-    } else {
-        include("./cfg/connect.prod.inc.php");
-    }
+	define('DB_HOST', 'localhost'); // database host
+	define('DB_USER', 'ruslan'); // username
+	define('DB_PASS', 'ruslan123'); // password
+	define('DB_NAME', 'shop'); // database name
+	define('ADMIN_LOGIN', 'YWRtaW4='); //administrator's login
+	define('ADMIN_PASS', '01e20b61d05bb6b42840997233579e08'); //administrator's login
+
 	//database tables
 	include("./cfg/tables.inc.php");
 
